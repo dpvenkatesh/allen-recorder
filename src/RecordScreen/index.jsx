a5243da473102b05de4a9800b5ac9d74a400d308
dpvenkatesh <dpven321@gmail.com>
2024-02-01 22:34:09 +0530

    Partial screen recording

--- src/RecordScreen/index.jsx
+++ src/RecordScreen/index.jsx
@@ -1,4 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 export const RecordScreen = () => {
-    return <div>Record Screen</div>;
+
+
+    // create ref for steam
+    const stream = React.useRef(null);
+
+    const [capturedVideo, setCapturedVideo] = useState(null);
+    const [recordingStarted, setRecordingStarted] = useState(false);
+
+    async function startRecording() {
+        stream.current = await navigator.mediaDevices.getDisplayMedia({
+            video: { mediaSource: "screen" }
+        });
+        const recorder = new MediaRecorder(stream.current);
+
+        const chunks = [];
+        recorder.ondataavailable = e => chunks.push(e.data);
+        recorder.onstop = e => {
+            const completeBlob = new Blob(chunks, { type: chunks[0].type });
+            setCapturedVideo(URL.createObjectURL(completeBlob));
+        };
+
+        recorder.start();
     }
+
+    return <div>
+
+        <button onClick={startRecording}>Start Recording</button>
+
+        {capturedVideo && (
+            <a href={capturedVideo} download="recorded-video.webm" className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded ml-4">Download</a>
+        )}
+
+    </div>;
+}
